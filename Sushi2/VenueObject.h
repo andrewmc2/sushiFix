7077c6448c7f51d8b239740d1e68eed3250edb9a
Rehmat Qadir <rehmatqadir@gmail.com>
2013-05-29 16:58:40 -0500

    map start

---
+++ Sushi2/VenueObject.h
@@ -0,0 +1,13 @@
+//
+//  VenueObject.h
+//  Sushi2
+//
+//  Created by MasterRyuX on 5/29/13.
+//  Copyright (c) 2013 Andrew McCallum. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VenueObject : NSObject
+
+@end
