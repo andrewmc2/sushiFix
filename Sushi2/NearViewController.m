4e9fbebb5673560c2e4d8c09a3ed86283f279a28
Craig Pellegrini <craig.pellegrini@live.com>
2013-05-30 00:02:13 -0500

    Added compass code

--- Sushi2/NearViewController.m
+++ Sushi2/NearViewController.m
@@ -14,6 +14,9 @@
     CLLocationManager *locationManager;
 }
 
+@property (strong, nonatomic) IBOutlet UIImageView *needle;
+
+
 @end
 
 @implementation NearViewController
@@ -27,14 +30,21 @@
     return self;
 }
 
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [self StartStandardlocationServices];
+    [self StartStandardLocationServices];
 
-    
+    self.needle = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"chopstickBowl.jpg"]];
+    self.needle.frame = CGRectMake(10, 10, 70, 70);
+    self.needle.backgroundColor = [UIColor clearColor];
+    self.needle.opaque = NO;
+
+    self.needle = self.needle;
     
 }
+
 
 - (void)didReceiveMemoryWarning
 {
@@ -43,13 +53,10 @@
 }
 
 
--(void) StartStandardlocationServices
+-(void) StartStandardLocationServices
 {
-    // Create the location manager if this object does not
-    // already have one.
-    
     if (nil == locationManager)
-        locationManager = [[CLLocationManager alloc] init];
+    locationManager = [[CLLocationManager alloc] init];
     
     locationManager.delegate = self;
     locationManager.desiredAccuracy = kCLLocationAccuracyKilometer;
@@ -58,6 +65,12 @@
     locationManager.distanceFilter = 500;
     
     [locationManager startUpdatingLocation];
+
+    if([CLLocationManager headingAvailable]) {
+        [locationManager startUpdatingHeading];
+    } else {
+        NSLog(@"No Compass -- You're lost");
+    }
 }
 
 
@@ -68,15 +81,33 @@
     NSDate* eventDate = location.timestamp;
     NSTimeInterval howRecent = [eventDate timeIntervalSinceNow];
     if (abs(howRecent) < 15.0) {
+    
     // If the event is recent, do something with it.
     NSLog(@"latitude %+.6f, longitude %+.6f\n", location.coordinate.latitude, location.coordinate.longitude);
     }
 }
 
+
 -(void) locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
 {
     NSLog(@"the error is %@", error);
 }
+
+
+-(void) locationManager:(CLLocationManager *)manager didUpdateHeading:(CLHeading *)newHeading
+{
+    NSLog(@"Magnetic heading %f", newHeading.magneticHeading);
+    //If the value is 0, pointing magnetic north, 90 means east, 180 is south, etc.
+    
+    double degrees = newHeading.magneticHeading;
+    double radians = degrees * M_PI / 180;
+    self.needle.transform = CGAffineTransformMakeRotation(-radians);
+    
+    NSLog(@"New magnetic heading: %f", newHeading.magneticHeading);
+    NSLog(@"New true heading: %f", newHeading.trueHeading);
+    
+}
+
 
 
 
