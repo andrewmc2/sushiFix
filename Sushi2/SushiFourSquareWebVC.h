2304161ddbeec2a7105e363a5dc205349f34ee22
Rehmat Qadir <rehmatqadir@gmail.com>
2013-05-31 16:00:38 -0500

    new vc class for foursquareVC with code

---
+++ Sushi2/SushiFourSquareWebVC.h
@@ -0,0 +1,13 @@
+//
+//  SushiFourSquareWebVC.h
+//  Sushi2
+//
+//  Created by MasterRyuX on 5/31/13.
+//  Copyright (c) 2013 Andrew McCallum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SushiFourSquareWebVC : UIViewController
+
+@end
---
+++ Sushi2/SushiFourSquareWebVC.m
@@ -0,0 +1,38 @@
+//
+//  SushiFourSquareWebVC.m
+//  Sushi2
+//
+//  Created by MasterRyuX on 5/31/13.
+//  Copyright (c) 2013 Andrew McCallum. All rights reserved.
+//
+
+#import "SushiFourSquareWebVC.h"
+
+@interface SushiFourSquareWebVC ()
+
+@end
+
+@implementation SushiFourSquareWebVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
